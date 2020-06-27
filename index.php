46d615cb63611e44b4c818b8c98ea1f5d3a0a157
wstiq <ritterwolf1942@mail.ru>
2020-06-28 02:09:46 +0300

    300 e1 stack sizes completed

--- index.php
+++ index.php
@@ -3,10 +3,10 @@ if (isset($_REQUEST['stack']) && isset($_REQUEST['pos'])) {
     $stack = $_REQUEST['stack'];
     $pos = $_REQUEST['pos'];
     pictureOutput($stack, $pos);
-    echo ($pos);
+    //echo($pos);
 } else {
     include("index.html");
-    echo("test123");
+    //echo("test123");
 }
 
 function pictureOutput($stack, $pos)
@@ -15,13 +15,43 @@ function pictureOutput($stack, $pos)
         case '15':
             switch ($pos) {
                 case 'button':
-                    echo('GTO push for 15 bb when hero button');
-                    //вывод пикчи
-                    //echo(imagepng(imagecreatefrompng("Screenshot_2.png")));
+                    echo('15 bb | hero — button'); ?>
+                    <a href="http://localhost:63342/somesortafun/index.php?stack=15&pos=button&send=%D0%9E%D1%82%D0%BF%D1%80%D0%B0%D0%B2%D0%B8%D1%82%D1%8C"><img
+                                src="BTN_15bbeff.png"/></a>
+                    <?php
                     break;
                 case 'sb(bu fold)':
-                    echo('sb(bu fold)');
-                    //вывод пикчи
+                    echo('15 bb | hero — sb | bu fold'); ?>
+                    <a href="http://localhost:63342/somesortafun/index.php?stack=15&pos=sb%28bu+fold%29&send=%D0%9E%D1%82%D0%BF%D1%80%D0%B0%D0%B2%D0%B8%D1%82%D1%8C"><img
+                                src="SB_15bbeff.png"/></a>
+                    <?php
+                    break;
+                case 'sb(bu push)':
+                    echo('15 bb | hero — sb | bu push'); ?>
+                    <a href="http://localhost:63342/somesortafun/index.php?stack=15&pos=sb%28bu+push%29&send=%D0%9E%D1%82%D0%BF%D1%80%D0%B0%D0%B2%D0%B8%D1%82%D1%8C"><img
+                                src="BTN_15bbeff_SB_call.png"/></a>
+                    <?php
+                    break;
+                case 'bb(bu push, sb call)':
+                    echo('15 bb | hero — bb | bu push | sb push'); ?>
+                    <a href="http://localhost:63342/somesortafun/index.php?stack=15&pos=bb%28bu+push%2C+sb+call%29&send=%D0%9E%D1%82%D0%BF%D1%80%D0%B0%D0%B2%D0%B8%D1%82%D1%8C"><img
+                                src="BTN_15bbeff_SB_call_BB_overcall.png"/></a>
+                    <?php
+                    break;
+                case 'bb(bu push, sb fold)':
+                    echo('15 bb | hero — bb | bu push | sb fold'); ?>
+                    <a href="http://localhost:63342/somesortafun/index.php?stack=15&pos=bb%28bu+push%2C+sb+fold%29&send=%D0%9E%D1%82%D0%BF%D1%80%D0%B0%D0%B2%D0%B8%D1%82%D1%8C"><img
+                                src="BTN_15bbeff_BB_call.png"/></a>
+                    <?php
+                    break;
+                case  'bb(bu fold, sb push)':
+                    echo('15 bb | hero — bb | bu fold | sb push'); ?>
+                    <a href="http://localhost:63342/somesortafun/index.php?stack=15&pos=bb%28bu+fold%2C+sb+push%29&send=%D0%9E%D1%82%D0%BF%D1%80%D0%B0%D0%B2%D0%B8%D1%82%D1%8C"><img
+                                src="SB_15bbeff_BB_call.png"/></a>
+                    <?php
+                    break;
+                default :
+                    echo('no way');
                     break;
             }
             break;
@@ -29,15 +59,17 @@ function pictureOutput($stack, $pos)
             switch ($pos) {
                 case 'button':
                     echo('GTO push for 12 bb when hero button');
-                    //вывод пикчи
+//вывод пикчи
                     break;
                 case 'sb(bu fold)':
                     echo('sb(bu fold)');
-                    //вывод пикчи
+//вывод пикчи
                     break;
             }
             break;
         default:
-            echo ("а я не придумал че екоу");
+            echo("а я не придумал че екоу");
     }
 }
+
+?>
